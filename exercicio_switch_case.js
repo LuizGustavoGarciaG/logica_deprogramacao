8bb0bef1d955d8e95a9236278ae83bd5a31fef48
Luiz <luiz.gustavolggom@gmail.com>
2025-05-29 11:43:13 -0300

    segundo switch case

---
+++ exercicio_switch_case.js
@@ -0,0 +1,243 @@
+/*let saudacao = &quot;Portugues&quot;
+switch(saudacao){
+case &quot;Portugues&quot;:
+console.log(&quot;Bom dia&quot;)
+break
+case &quot;Espanhol&quot;:
+console.log (&quot;Bueno Dias&quot;)
+break
+case &quot;Ingles&quot;:
+console.log(&quot;Good morning&quot;)
+break
+default:
+console.log(&quot;Esse idioma não está registrado&quot;)
+}
+let diaDaSemana = 4
+let nomeDia
+
+switch(diaDaSemana) {
+case 1:
+nomeDia = &quot;Domingo&quot;
+break
+case 2:
+console.log(nomeDia = &quot;Segunda&quot;)
+break
+case 3:
+console.log(nomeDia = &quot;Terça&quot;)
+break
+case 4:
+console.log(nomeDia = &quot;Quarta&quot;)
+break
+case 5:
+
+console.log(nomeDia = &quot;Quinta&quot;)
+break
+case 6:
+console.log(nomeDia = &quot;Sexta&quot;)
+break
+case 7:
+console.log(nomeDia = &quot;Sábado&quot;)
+break
+default:
+console.log(nomeDia = &quot;Dia inválido&quot;)
+}
+let transporte = 2
+let tipoTransporte;
+
+switch(transporte) {
+case 1:
+tipoTransporte = &quot;Carro&quot;
+break
+case 2:
+tipoTransporte = &quot;Bicicleta&quot;
+break
+case 3:
+tipoTransporte = &quot;Ônibus&quot;
+break
+default:
+tipoTransporte = &quot;Transporte desconhecido&quot;
+}
+
+console.log(tipoTransporte)
+let urgencia = 3
+let nivelUrgencia
+
+switch(urgencia) {
+case 1:
+nivelUrgencia = &quot;Baixa&quot;
+break
+case 2:
+nivelUrgencia = &quot;Média&quot;
+break
+case 3:
+nivelUrgencia = &quot;Alta&quot;
+break
+default:
+nivelUrgencia = &quot;Nível desconhecido&quot;
+}
+
+console.log(nivelUrgencia)
+let categoria = &quot;SUV&quot;
+let tipoVeiculo;
+
+switch(categoria) {
+case &quot;compacto&quot;:
+tipoVeiculo = &quot;Veículo compacto&quot;
+break
+case &quot;SUV&quot;:
+tipoVeiculo = &quot;Veículo utilitário esportivo&quot;
+break
+case &quot;caminhão&quot;:
+tipoVeiculo = &quot;Caminhão de carga&quot;
+break
+default:
+tipoVeiculo = &quot;Categoria desconhecida&quot;
+
+}
+
+console.log(tipoVeiculo)*/
+/*let tempo = 3
+let tarifa
+
+if (tempo &lt;= 1) {
+tarifa = 5
+} else if (tempo &lt;= 3) {
+tarifa = 10
+} else {
+tarifa = 15
+}
+
+console.log(`Tarifa: R$${tarifa}`)*/
+/*let dificuldade = 2
+let nivel
+
+switch(dificuldade) {
+case 1:
+nivel = &quot;Fácil&quot;
+break
+case 2:
+nivel = &quot;Médio&quot;
+break
+case 3:
+nivel = &quot;Difícil&quot;
+break
+default:
+nivel = &quot;Desconhecido&quot;
+}
+
+console.log(nivel)*/
+/*let servico = &quot;musculação&quot;
+let tipoServico
+
+switch(servico) {
+case &quot;aula de yoga&quot;:
+tipoServico = &quot;Aulas de Yoga&quot;
+break
+case &quot;musculação&quot;:
+tipoServico = &quot;Área de Musculação&quot;
+break
+case &quot;personal trainer&quot;:
+tipoServico = &quot;Acompanhamento com Personal Trainer&quot;
+break
+default:
+tipoServico = &quot;Serviço indisponível&quot;
+}
+
+console.log(tipoServico)*/
+/*let categoriaIngresso = &quot;meia entrada&quot;
+let preco
+
+switch(categoriaIngresso) {
+case &quot;estudante&quot;:
+preco = 15
+break
+case &quot;meia entrada&quot;:
+preco = 20
+break
+case &quot;inteira&quot;:
+
+preco = 40
+break
+default:
+preco = 0
+}
+
+console.log(`Preço: R$${preco}`)*/
+/*let idade = 65
+let dificuldade
+
+if (idade &lt; 18) {
+dificuldade = &quot;Leve&quot;
+} else if (idade &lt;= 50) {
+dificuldade = &quot;Moderada&quot;
+} else {
+dificuldade = &quot;Suave&quot;
+}
+console.log(dificuldade)*/
+/*let genero = &quot;rock&quot;
+let instrumento
+
+switch(genero) {
+case &quot;rock&quot;:
+instrumento = &quot;Guitarra&quot;
+break
+case &quot;clássico&quot;:
+instrumento = &quot;Violino&quot;
+break
+case &quot;samba&quot;:
+instrumento = &quot;Pandeiro&quot;
+break
+
+default:
+instrumento = &quot;Instrumento desconhecido&quot;
+}
+
+console.log(instrumento)*/
+/*let area = &quot;back-end&quot;;
+let curso;
+
+switch(area) {
+case &quot;front-end&quot;:
+curso = &quot;Curso de HTML, CSS e JavaScript&quot;;
+break;
+case &quot;back-end&quot;
+curso = &quot;Curso de Node.js e Banco de Dados&quot;
+break
+case &quot;mobile&quot;:
+curso = &quot;Curso de React Native&quot;
+break
+default:
+curso = &quot;Área não encontrada&quot;
+}
+
+console.log(curso)*/
+/*let idadeAtleta = 22
+let nivelHabilidade
+
+if (idadeAtleta &lt; 12) {
+nivelHabilidade = &quot;Iniciante&quot;
+} else if (idadeAtleta &lt; 18) {
+nivelHabilidade = &quot;Intermediário&quot;
+} else {
+
+nivelHabilidade = &quot;Avançado&quot;
+}
+console.log(nivelHabilidade)*/
+/*let categoria = &quot;intermediário&quot;;
+let dificuldade;
+
+switch(categoria) {
+case &quot;básico&quot;:
+dificuldade = &quot;Desafio fácil&quot;
+break
+case &quot;intermediário&quot;:
+dificuldade = &quot;Desafio médio&quot;
+break
+case &quot;avançado&quot;:
+dificuldade = &quot;Desafio difícil&quot;;
+break
+default:
+dificuldade = &quot;Categoria inválida&quot;
+}
+
+console.log(dificuldade)*/
