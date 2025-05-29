3f3fb411ef51e1d479c006be29ccada763bd8351
Luiz <luiz.gustavolggom@gmail.com>
2025-05-29 10:57:04 -0300

    variaveis

---
+++ variaveis.js
@@ -0,0 +1,50 @@
+// Meu primeiro codigo
+
+// Declarando uma variável e exibindo no console
+let nome = "Luiz";
+console.log(nome);
+
+// Declarando uma variável e exibindo no console
+let idade = "16";
+console.log(idade);
+
+// Criando uma variável booleana
+let aprendendoJavaScript = "true";
+console.log(aprendendoJavaScript);
+
+// Concatenando strings
+let primeironome = "Luiz ";
+let segundonome = "Gustavo ";
+let terceironome = "Garcia";
+console.log(primeironome + segundonome + terceironome);
+
+// Somando dois números
+// let numero1 = "10";
+// let numero2 = "20";
+// let soma = (numero1) + (numero2);
+console.log(20 + 10);
+
+// Multiplicando dois números
+let numero1 = "10";
+let numero2 = "20";
+let multiplicacao = (numero1 * numero2);
+console.log(20 * 10);
+
+// Variável de texto e número
+let cidade = "Ferraz de Vasconcelos";
+console.log(cidade);
+let habitantes = "179.198";
+console.log(habitantes);
+
+// Alterando o valor de uma variável
+let preco = 50;
+preco += 30;
+console.log(preco);
+
+// Criando uma variável sem valor (undefined)
+let meuValor;
+console.log(meuValor);
+
+// Criando um valor null
+let meuNulo = null;
+console.log(null);
