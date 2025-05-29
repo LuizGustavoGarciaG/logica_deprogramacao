18c488fa9992f2074da2f2ed7d06b5fb29bed39a
Luiz <luiz.gustavolggom@gmail.com>
2025-05-29 11:04:11 -0300

    programas

---
+++ meu_programa.js
@@ -0,0 +1,28 @@
+let comeco = "Bem vindo";
+let login = "Entrar com email";
+let confirmacao = "Confirmar";
+console.log(comeco);
+console.log(login);
+console.log(confirmacao);
+
+let email = prompt("Digite seu email: ");
+let senha = Number(prompt("Digite sua senha: "));
+
+let confirmacao1 = "Confirmar senha";
+console.log(confirmacao1);
+
+let confirmacao2 = "Entrar";
+console.log(confirmacao2);
+
+let comprar = "Clique nos três pontos do canto da tela e vá até ao ícone de carrinho";
+console.log(comprar);
+
+let comprar1 = "Selecione a roupa de seu agrado";
+let comprar2 = "Escolha o tamanho que deseja e adicione ao carrinho";
+console.log(comprar1);
+console.log(comprar2);
+
+let pagto = prompt("Coloque qual método deseja pagar: ");
+
+let final = "Sua compra foi finalizada, volte para o início";
+console.log(final);
