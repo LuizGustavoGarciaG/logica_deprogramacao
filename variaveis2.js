3244558a79381a332018a939c35614fdd21b9979
Luiz <luiz.gustavolggom@gmail.com>
2025-05-29 11:09:34 -0300

    variaveis2

---
+++ variaveis2.js
@@ -0,0 +1,12 @@
+// let nome = prompt("Digite seu nome: ")
+// let idade = Number(prompt("Digite a sua idade: "))
+// console.log("O seu nome", nome, "você tem", idade, "anos")
+
+// let nome = prompt("Digite seu nome: ")
+// let idade = Number(prompt("Digite a sua idade: "))
+// let email = prompt("Digite seu email: ")
+// let telefone = Number(prompt("Digite seu telefone: "))
+// console.log("O seu nome", nome, "você tem", idade, "anos.", "O seu endereço de email é", email, "e o seu número de telefone é", telefone)
+
+// let nascimento = Number(prompt("Digite seu ano de nascimento: "))
+// console.log(2025 - nascimento)
